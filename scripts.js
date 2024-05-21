f18d331728da03c80d355af6a7079503c16fb0ae
edwinhar64 <50516463+edwinhar64@users.noreply.github.com>
2024-05-21 09:21:54 -0500

    Envio de Modelo

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function enviar() {
+    alert("Mensaje enviado");
+}
+
+function FLimpiar() {
+    document.querySelector('.input1').value = 'nombre...';
+    document.querySelector('.input2').value = 'imagen...';
+    document.querySelector('.input3').value = 'precio...';
+}
+
+function FEliminar() {
+    alert("Producto eliminado");
+}
